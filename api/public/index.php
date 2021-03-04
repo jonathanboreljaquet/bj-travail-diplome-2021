609988f5f42bf603c3590fc25599414f9e33141a
jonathanboreljaquet <jonathan.brljq@eduge.ch>
2021-03-04 13:19:58 +0100

    Update index.php

--- api/public/index.php
+++ api/public/index.php
@@ -13,7 +13,7 @@ $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'mail.infomaniak.com';                     //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 $mail->Username   = 'dev@boreljaquet.ch';                     //SMTP username
-$mail->Password   = 'DsJSH_HNhR8';                               //SMTP password
+$mail->Password   = '';                               //SMTP password
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 $mail->Port       = 587; 
 
